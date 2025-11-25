41dce2bd23d7162e69b903222fb2187f7ed3526c
sethumadhavan7 <chandrabosechandrabose9@gmail.com>
2025-11-25 18:04:02 +0530

    Increase THRESHOLD value from 1 to 100

--- server/routes/gasRoutes.js
+++ server/routes/gasRoutes.js
@@ -71,7 +71,7 @@ router.post('/', async (req, res) => {
     // 🔥 AUTO CONTROL:
     // If gasValue > THRESHOLD  → CLOSED
     // If gasValue <= THRESHOLD → OPEN
-    const THRESHOLD = 1;
+    const THRESHOLD = 100;
     let knobStatus = gasValue > THRESHOLD ? 'CLOSED' : 'OPEN';
 
     // ✅ SMS only when it CHANGES to CLOSED (avoid spam)
