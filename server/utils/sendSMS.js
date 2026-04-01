e689cb860eed5c10d67e40a01a5ed41de0744555
sethumadhavan7 <chandrabosechandrabose9@gmail.com>
2026-04-01 06:49:18 +0530

    Refactor sendSMS function for better error handling
    Updated SMS sending logic to log success and prevent errors from crashing the API.

--- server/utils/sendSMS.js
+++ server/utils/sendSMS.js
@@ -1,22 +1,59 @@
+// const twilio = require('twilio');
+
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
+// const client = twilio(accountSid, authToken);
+
+// const sendSMS = async (message, to) => {
+//   try {
+//     const res = await client.messages.create({
+//       body: message,
+//       from: twilioNumber,
+//       to: to
+//     });
+//     return res;
+//   } catch (err) {
+//     console.error('SMS sending failed:', err.message);
+//     throw err;
+//   }
+// };
+
+// module.exports = sendSMS; 
+
 const twilio = require('twilio');
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
+
 const client = twilio(accountSid, authToken);
 
 const sendSMS = async (message, to) => {
-  try {
-    const res = await client.messages.create({
-      body: message,
-      from: twilioNumber,
-      to: to
-    });
-    return res;
-  } catch (err) {
-    console.error('SMS sending failed:', err.message);
-    throw err;
-  }
+try {
+
+```
+const res = await client.messages.create({
+  body: message,
+  from: twilioNumber,
+  to: to
+});
+
+console.log("SMS sent:", res.sid);
+return res;
+```
+
+} catch (err) {
+
+```
+console.error("SMS sending failed:", err.message);
+
+// Do NOT throw error
+// This prevents the API from crashing
+return null;
+```
+
+}
 };
 
-module.exports = sendSMS; 
+module.exports = sendSMS;
